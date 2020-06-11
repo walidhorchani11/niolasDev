45d22d43bb71ee6d09386a60c8a56d6bdac276bc
walid horchani <walid.horchani@oyez.fr>
2020-06-11 14:02:41 +0100

    AddPlaceForm component

---
+++ components/AddPlaceForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+
+const AddPlaceForm = (props) => {
+  const [places, setPlaces] = useState([0]);
+
+  const displayPlacesInput = () => {
+    return places.map((place, indice) => (
+      <View key={indice}>
+        <Text style={styles.label}>{`Lieu ${indice + 1}`}</Text>
+        <TextInput
+          style={styles.input}
+          value={places[indice]}
+          onChangeText={(val) => {}}
+        ></TextInput>
+      </View>
+    ));
+  };
+
+  return (
+    <View style={styles.formControl}>
+      {displayPlacesInput()}
+      <Button title="ajouter lieu" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  formControl: {},
+  label: {},
+  input: { borderBottomWidth: 1, borderBottomColor: 'grey' },
+  form: {},
+});
+
+export default AddPlaceForm;

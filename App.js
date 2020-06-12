92657378b9d4bcc5d67b81c0d4c6eb5624addaa6
walid horchani <walid.horchani@oyez.fr>
2020-06-13 00:01:58 +0100

    refact

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Home from './screens/Home';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Home />
+      <HomeScreen />
     </View>
   );
 }
--- screens/ManageProductScreen.js
+++ screens/ManageProductScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
-const ManageProductScree = () => {
+const ManageProductScreen = () => {
   return (
     <View style={styles.screen}>
       <Text>Screen for adding & deleting product</Text>
@@ -18,4 +18,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ManageProductScree;
+export default ManageProductScreen;

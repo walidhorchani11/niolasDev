85f0066e947074dc7cab87822e8ed6026bd26ca2
walid horchani <walid.horchani@oyez.fr>
2020-06-14 16:34:31 +0100

    navigate to manageProduct screen after taking pic

--- App.js
+++ App.js
@@ -1,16 +1,7 @@
 import React from 'react';
 // import { StyleSheet, View } from 'react-native';
-// import HomeScreen from './screens/HomeScreen';
 import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
   return <AppNavigator />;
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });

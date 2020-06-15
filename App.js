bb66b45b014c34f9171b84d1f871b487c1ab975c
walid horchani <walid.horchani@oyez.fr>
2020-06-15 17:13:29 +0100

    load font

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppNavigator from './navigation/AppNavigator';
-
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'OpenSans-Bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    'OpenSans-Regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+  });
+};
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
   return <AppNavigator />;
 }
